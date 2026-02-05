c283413cb907647ca9dddf567f2a15aa96b3008a
krishkumar6 <krishkumarmath@gmail.com>
2026-02-05 18:03:59 +0530

    custom api and error handle

--- src/index.js
+++ src/index.js
@@ -3,4 +3,13 @@ import connectDB from "./db/index.js";
 
 dotenv.config({ path: ".env" });
 
+.then(() => {
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`server is runninng ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGODB is failed !!!" , err);
+})
+
 connectDB();
